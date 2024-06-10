cc576862d6e471bdd5469b30da8bc87a8cb96e48
Bunyaminpilsim <bunyaminpilsim@gmail.com>
2024-06-10 17:03:41 +0300

    created porıject

---
+++ LibraryManagmentSystem/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryManagmentSystem.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public IFormFile? File { get; set; }
+        public string? CoverImgPath { get; set; }
+        public int PageCount { get; set; }
+        public int CategoryId { get; set; }
+        public List<Category>? Categories { get; set;}
+    }
+}
---
+++ LibraryManagmentSystem/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using LibraryManagmentSystem.Models;
+
+namespace LibraryManagmentSystem.Repositories
+{
+    public class CategoryRepository:ICategoryRepository
+    {
+        List<Category> _categories=new List<Category>();
+
+        public void AddCategory(Category category)
+        {
+           _categories.Add(category);
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            _categories.Remove(category);
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _categories;
+        }
+
+        public Category GetCategoryById(int id)
+        {
+           var category = _categories.FirstOrDefault(c => c.Id == id);
+
+            return category;
+        }
+
+        public void UpdateCategory(Category category)
+        {
+           var exCategory=GetCategoryById(category.Id);
+            if (exCategory != null)
+            {
+                exCategory.CategoryName=category.CategoryName;
+            }
+        }
+    }
+}
